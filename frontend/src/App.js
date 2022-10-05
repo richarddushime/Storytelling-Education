16a35ba1e1859bef272ea64b07e028d54dd505d2
Holytech <akeemqudus2016@gmail.com>
2022-10-05 16:03:22 +0100

    Set up the react project for the frontend, also made edited the folder structure for architectural reasons

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl italic">Storytelling-Education</h1>
+      <h1 className="text-3xl font-bold underline">Project set up complete</h1>
+    </div>
+  );
+}
+
+export default App;
